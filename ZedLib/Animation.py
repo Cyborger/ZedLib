954c42fc35cd055c29dff1cdc67c0fbe4a572b5e
Cyborger <joe.zlonicky@gmail.com>
2017-05-08 13:07:29 -0700

    Started working on the class

--- ZedLib/Animation.py
+++ ZedLib/Animation.py
@@ -1,1 +1,9 @@
+import Spritesheet
 
+class Animation:
+  def __init__(self, sprite, frames):
+    self.sprite = sprite
+    self.frames = frames
+    self.current_frame_n = 0
+    self.frame_image = None
+    
