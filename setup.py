b972ff1c2e30fd7410e66640a12d1aa4172a60f9
Cyborger <joe.zlonicky@gmail.com>
2017-05-10 14:10:54 -0700

    Moved test folder
    Updated setup to v0.2, can't update it currently to test

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ZedLib',
-      version='0.1',
+      version='0.2',
       description='A library built on top of pygame',
       author='Joe Zlonicky',
       packages=['ZedLib'],
