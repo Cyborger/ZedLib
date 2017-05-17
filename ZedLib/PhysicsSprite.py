adbca944ad7d26bcae047896314d8ee911259d77
Cyborger <joe.zlonicky@gmail.com>
2017-05-17 15:15:42 -0700

    Revert "Revert "4563516162Starte""
    This reverts commit 9962e82a62727d3508b5567a0d8f291431eff08f.

--- ZedLib/PhysicsSprite.py
+++ ZedLib/PhysicsSprite.py
@@ -1,3 +1,14 @@
 """ A GameSprite that has gravity"""
-class PhysicsSprite:
-    pass
+import ZedLib
+
+class PhysicsSprite(ZedLib.GameSprite):
+    def __init__(self, image, x=0, y=0):
+        super().__init__(image, x, y)
+        self.gravity = 0.1
+        self.y_velocity = 0.0
+
+    def UpdateGravity(self):
+        self.y_velocity += self.gravity
+
+    def HitGround(self):
+        self.y_velocity = 0.0
