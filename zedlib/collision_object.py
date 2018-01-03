bf896354ca6ec29f7e5369bb88a980e384f8f7cc
Cyborger <joe.zlonicky@gmail.com>
2018-01-03 11:19:06 -0800

    Updated docstring

--- zedlib/collision_object.py
+++ zedlib/collision_object.py
@@ -6,14 +6,14 @@ class CollisionObject:
         self.rect = pygame.Rect(x, y, width, height)
 
     def horizontal_collide(self, sprite):
-        """ Stops sprite from walking through this object on x-axis """
+        """ Stops GameSprite from going through this object on x-axis """
         if sprite.move_x > 0.0:
             sprite.rect.right = self.rect.left
         elif sprite.move_x < 0.0:
             sprite.rect.left = self.rect.right
 
     def vertical_collide(self, sprite):
-        """ Stops sprite from going through this object on y-axis """
+        """ Stops GameSprite from going going this object on y-axis """
         if sprite.move_y > 0.0:
             sprite.rect.bottom = self.rect.top
         elif sprite.move_y < 0.0:
