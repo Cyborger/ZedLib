0cfb44aa4289bc588e6f870cef5af4185941cb96
Cyborger <joe.zlonicky@gmail.com>
2018-01-04 12:02:54 -0800

    Organized

--- ZedLib/Game.py
+++ ZedLib/Game.py
@@ -9,11 +9,12 @@ class Game:
         pygame.init()
         self.screen_width = screen_width
         self.screen_height = screen_height
-        self.screen_middle = [self.screen_width /2, self.screen_height / 2]
+        self.screen_size = (self.screen_width, self.screen_height)
         self.screen = pygame.Surface((self.screen_width, self.screen_height))
-        self.render_window = render_window.RenderWindow(self.screen_width,
-                                                        self.screen_height,
-                                                        fullscreen)
+
+        self.render_window = render_window.\
+                RenderWindow(self.screen_width, self.screen_height, fullscreen)
+                
         self.current_state = game_state.GameState(self)
         self.clock = pygame.time.Clock()
         self.running = True
@@ -25,17 +26,37 @@ class Game:
     def loop(self):
         """ Calls main functions every frame """
         while self.running:
-            self.current_state.handle_events()
+            self.current_state.handle_events(pygame.event.get())
             self.current_state.handle_input()
             self.current_state.update()
-            self.current_state.clear_screen()
+            self.clear_screen()
             self.current_state.draw_screen()
             self.update_display()
             self.current_state.handle_fps()
 
+    def clear_screen(self):
+        self.screen.fill((0, 0, 0))
+
     def update_display(self):
-        """ Scale the screen then update the screen """
-        dimensions = self.render_window.get_dimensions()
+        """ Scale the screen then update the display """
+        dimensions = self.render_window.get_window_size()
         scaled_screen = pygame.transform.scale(self.screen, dimensions)
         self.render_window.screen.blit(scaled_screen, (0, 0))
+
         pygame.display.flip()
+
+    def get_mouse_position(self):
+        """ Get scaled position of the mouse """
+        unscaled_mouse_pos = pygame.mouse.get_pos()
+
+        screen_w = self.screen_width
+        render_screen_w = self.render_window.get_window_size()[0]
+        x_modifier = screen_w / render_screen_w
+        x_pos = unscaled_mouse_pos[0] * x_modifier
+
+        screen_h = self.screen_height
+        render_screen_h = self.render_window.get_window_size()[1]
+        y_modifier = screen_h / render_screen_h
+        y_pos = unscaled_mouse_pos[1] * y_modifier
+
+        return (x_pos, y_pos)
