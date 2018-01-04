fbd7174f1fb91a8032cbe9f9966c037fec3beb10
Cyborger <joe.zlonicky@gmail.com>
2018-01-04 12:04:34 -0800

    Cleaned up

--- zedlib/image_loading.py
+++ zedlib/image_loading.py
@@ -5,7 +5,6 @@ def load_image(file_path, scale=1):
     """ Load an image, automatically calls convert_alpha() """
     image = pygame.image.load(file_path).convert_alpha()
     if scale is not 1:
-        new_width = image.get_width() * scale
-        new_height = image.get_height() * scale
-        image = pygame.transform.scale(image, (new_width, new_height))
+        new_size = (image.get_width() * scale, image.get_height() * scale)
+        image = pygame.transform.scale(image, new_size)
     return image
