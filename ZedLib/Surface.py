aefde43a552c2c887ce89c4aedea7f1e9c9beb2d
Cyborger <joe.zlonicky@gmail.com>
2018-01-04 12:04:56 -0800

    Added centering

--- ZedLib/Surface.py
+++ ZedLib/Surface.py
@@ -2,15 +2,22 @@ import zedlib
 import pygame
 
 
-#TODO: Add some features
-
 class Surface:
     """ An image with a rect """
-    def __init__(self, image, x, y):
+    def __init__(self, image, x=0, y=0):
         self.image = image
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
 
-    def draw(self, screen):
-        screen.blit(self.image, self.rect)
+    def draw(self, surface):
+        """ Draw image on another surface """
+        surface.blit(self.image, self.rect)
+
+    def center_horizontal(self, rect):
+        """ Center rect horizontally by the center of a given rect """
+        self.rect.centerx = rect.centerx
+
+    def center_vertical(self, rect):
+        """ Center rect vertically by the center of a given rect """
+        self.rect.centery = rect.centery
